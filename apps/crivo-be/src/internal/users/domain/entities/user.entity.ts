917c15b149cbe5d97ef89e4685b037a96dad8f3b
Geraldo Luiz <geraldobl58@gmail.com>
2026-04-15 18:32:50 -0300

    feat(user): refactor UserEntity to use private properties and improve encapsulation

--- apps/crivo-be/src/internal/users/domain/entities/user.entity.ts
+++ apps/crivo-be/src/internal/users/domain/entities/user.entity.ts
@@ -1,16 +1,15 @@
 import { Role } from '../enums/user.role.enum';
 
 export class UserEntity {
-  readonly id: string;
-  readonly keycloakId: string;
-  readonly email: string;
-  readonly firstname: string | null;
-  readonly lastname: string | null;
-  readonly role: Role;
-  readonly companyId: string;
-
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
+  private _id: string;
+  private _keycloakId: string;
+  private _email: string;
+  private _firstname: string | null;
+  private _lastname: string | null;
+  private _role: Role;
+  private _companyId: string | null;
+  private _createdAt: Date;
+  private _updatedAt: Date;
 
   constructor(props: {
     id: string;
@@ -19,18 +18,74 @@ export class UserEntity {
     firstname: string | null;
     lastname: string | null;
     role: Role;
-    companyId: string;
+    companyId: string | null;
     createdAt: Date;
     updatedAt: Date;
   }) {
-    this.id = props.id;
-    this.keycloakId = props.keycloakId;
-    this.email = props.email;
-    this.firstname = props.firstname;
-    this.lastname = props.lastname;
-    this.role = props.role;
-    this.companyId = props.companyId;
-    this.createdAt = props.createdAt;
-    this.updatedAt = props.updatedAt;
+    this._id = props.id;
+    this._keycloakId = props.keycloakId;
+    this._email = props.email;
+    this._firstname = props.firstname;
+    this._lastname = props.lastname;
+    this._role = props.role;
+    this._companyId = props.companyId;
+    this._createdAt = props.createdAt;
+    this._updatedAt = props.updatedAt;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get keycloakId(): string {
+    return this._keycloakId;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get firstname(): string | null {
+    return this._firstname;
+  }
+
+  set firstname(value: string | null) {
+    this._firstname = value;
+  }
+
+  get lastname(): string | null {
+    return this._lastname;
+  }
+
+  set lastname(value: string | null) {
+    this._lastname = value;
+  }
+
+  get role(): Role {
+    return this._role;
+  }
+
+  set role(value: Role) {
+    this._role = value;
+  }
+
+  get companyId(): string | null {
+    return this._companyId;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  set updatedAt(value: Date) {
+    this._updatedAt = value;
   }
 }
