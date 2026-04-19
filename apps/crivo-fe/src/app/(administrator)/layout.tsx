515085583ddad60309e35b75a814a4b2fc173a17
Geraldo Luiz <geraldobl58@gmail.com>
2026-04-19 10:17:45 -0300

    feat: wrap AdministratorLayout with SessionProvider for session management

--- apps/crivo-fe/src/app/(administrator)/layout.tsx
+++ apps/crivo-fe/src/app/(administrator)/layout.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react";
 import MuiProvider from "@/providers/MuiProvider";
 import { Wrapper } from "./components/Wrapper";
 import { QueryProvider } from "@/providers/QueryProvider";
@@ -8,10 +9,12 @@ export default function AdministratorLayout({
   children: React.ReactNode;
 }) {
   return (
-    <QueryProvider>
-      <MuiProvider>
-        <Wrapper>{children}</Wrapper>
-      </MuiProvider>
-    </QueryProvider>
+    <SessionProvider>
+      <QueryProvider>
+        <MuiProvider>
+          <Wrapper>{children}</Wrapper>
+        </MuiProvider>
+      </QueryProvider>
+    </SessionProvider>
   );
 }
